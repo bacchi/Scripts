9550e979d5a67491fcabe4290b1d446948020aef
bacchi <yuki1216pma@gmail.com>
2017-12-15 15:34:25 +0900

    Update md2html.pl

--- perl/md2html.pl
+++ perl/md2html.pl
@@ -1,5 +1,17 @@
 #!/usr/bin/env perl
 
+# to run this script, you have to install these modules according to the following procedure
+# cpanm Furl.pm
+# cpanm JSON.pm
+# cpanm Cache::LRU
+# cpanm Class::Load
+# cpanm IO::Socket::SSL
+# git clone git://github.com/Songmu/p5-Text-Markdown-GitHubAPI.git Text-Markdown-GitHubAPI
+# cd Text-Markdown-GitHubAPI
+# perl Makefile.PL
+# make
+# make install
+
 use strict;
 use warnings;
 use Text::Markdown::GitHubAPI 'markdown';
